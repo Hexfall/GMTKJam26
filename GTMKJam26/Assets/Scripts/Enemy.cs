4a0e8d12eb574259168501a20fb85cd9549ff76f
Viktor Mønster <viktor@mani.monster>
2026-07-24 18:54:01 +0200

    Reformatted enemy script to be more flexible and have more groundwork

--- GTMKJam26/Assets/Scripts/Enemy.cs
+++ GTMKJam26/Assets/Scripts/Enemy.cs
@@ -3,39 +3,142 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public GameObject player;
-    public bool followPlayer;
-    public float attackRange = 5f;
-    private NavMeshAgent agent;
+    public enum AgentStatus
+    {
+        Idle,
+        Hunting,
+        Attacking,
+        Staggered,
+        Dead,
+    }
+    private AgentStatus _status = AgentStatus.Idle;
+
+    public AgentStatus Status
+    {
+        get => _status;
+        set
+        {
+            _status = value;
+            switch (value)
+            {
+                case AgentStatus.Idle:
+                    // TODO: Play Idle animation
+                    break;
+                case AgentStatus.Hunting:
+                    // TODO: Play Walking animation
+                    break;
+                case  AgentStatus.Attacking:
+                    attackTime = 0.0f;
+                    // TODO: Play Attack animation
+                    break;
+                case AgentStatus.Staggered:
+                    staggeredTime = 0.0f;
+                    // TODO: Play Stagger animation
+                    break;
+                case AgentStatus.Dead:
+                    // TODO: Play death animation
+                    break;
+                default:
+                    break;
+            }
+            if (value != AgentStatus.Hunting)
+                agent.SetDestination(transform.position);
+            LogStatus();
+        }
+    }
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Hunting")]
+    private GameObject player;
+    private NavMeshAgent agent;
+
+    [Header("Defending")]
+    public float health = 100f;
+    public float staggerDuration = 1f;
+    private float staggeredTime = 0f;
+    
+    [Header("Attacking")]
+    public float attackRange = 5f;
+
+    private float attackRangeSqr = 25f;
+    public float attackDuration = 2f;
+    private float attackTime = 0f;
+    
     void Start()
     {
+        attackRangeSqr = attackRange * attackRange;
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HuntPlayer()
     {
-        if (followPlayer)
+        Status =  AgentStatus.Hunting;
+    }
+
+    void FixedUpdate()
+    {
+        switch (Status)
         {
-            agent.SetDestination(player.transform.position);
-            if (attackRange > (transform.position - player.transform.position).magnitude)
+            case AgentStatus.Idle:
+                Idle();
+                break;
+            case AgentStatus.Hunting:
+                Hunt();
+                break;
+            case  AgentStatus.Attacking:
                 Attack();
+                break;
+            case AgentStatus.Staggered:
+                Stagger();
+                break;
         }
-        else
-        {
-            Idle();
-        }
-    }
-
-    void Attack()
-    {
-        Debug.Log("Player was attacked");
     }
 
     void Idle()
     {
         agent.SetDestination(transform.position);
     }
+
+    void Hunt()
+    {
+        agent.SetDestination(player.transform.position);
+        if (attackRangeSqr > (transform.position - player.transform.position).sqrMagnitude)
+            Status = AgentStatus.Attacking;
+    }
+
+    void Attack()
+    {
+        attackTime += Time.fixedDeltaTime;
+        if (!(attackTime > attackDuration))
+            return;
+        
+        Status = AgentStatus.Hunting;
+        Debug.Log("Player was attacked");
+        // TODO: Make player take damage.
+    }
+
+    void Stagger()
+    {
+        staggeredTime += Time.fixedDeltaTime;
+        if (!(staggeredTime > staggerDuration))
+            return;
+
+        Status = AgentStatus.Hunting;
+    }
+
+    public void Damage(float damage)
+    {
+        health -= damage;
+        if (health <= 0f)
+            Die();
+        else if (Status != AgentStatus.Attacking)
+            Status = AgentStatus.Staggered;
+    }
+    
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void LogStatus() => Debug.Log($"Enemy '{gameObject.name}' is now in {Status} state.");
 }
