a5d80b51f331b464afcd1dd7acf636c619ed3b5c
Viktor Máni Mønster <viktor@mani.monster>
2026-07-26 15:37:34 +0200

    Healing should work and player heals after delay

--- GTMKJam26/Assets/Scripts/Damageable.cs
+++ GTMKJam26/Assets/Scripts/Damageable.cs
@@ -4,12 +4,54 @@ using UnityEngine.SceneManagement;
 
 public class Damageable : MonoBehaviour
 {
-    public UnityEvent OnDamage;
-    public UnityEvent OnDeath;
-    public float health = 100f;
+    [Header("Health")]
+    [SerializeField] private float health = 100f;
+    private float maxHealth;
+    
+    [Header("Healing")]
+    [SerializeField] private bool heals = false;
+    [SerializeField] [Range(0, 10)] private float healingRate = 2.5f;
+    [SerializeField] [Range(0, 10)] private float healingDelay = 5f;
+    private float sinceLastDamage = 0f;
+    private bool isHealing = false;
+    
+    [Header("Events")]
+    [SerializeField] public UnityEvent OnDamage;
+    [SerializeField] public UnityEvent OnDeath;
+    [SerializeField] public UnityEvent OnStartHealing;
+    [SerializeField] public UnityEvent OnFinishHealing;
 
+    private void Start()
+    {
+        maxHealth = health;
+    }
+
+    private void Update()
+    {
+        if (!heals)
+            return;
+
+        if (!isHealing)
+        {
+            sinceLastDamage += Time.deltaTime;
+            if (sinceLastDamage < healingDelay)
+                return;
+            isHealing = true;
+            OnStartHealing.Invoke();
+        }
+        
+        if (health >= maxHealth)
+            return;
+        
+        health = Mathf.Min(health + healingRate * Time.deltaTime, maxHealth);
+        if (health >= maxHealth)
+            OnFinishHealing.Invoke();
+    }
+    
     public void Damage(float damage)
     {
+        isHealing = false;
+        sinceLastDamage = 0f;
         OnDamage.Invoke();
         health -= damage;
         if (health <= 0f)
