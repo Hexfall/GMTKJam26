4da87c7dc1f02a888e77374eea99a316148bf710
ravnur376 <sjurur1@gmail.com>
2026-07-26 16:25:09 +0100

    Implemented shell pickup sfx

--- GTMKJam26/Assets/Scripts/Generic/Collectable.cs
+++ GTMKJam26/Assets/Scripts/Generic/Collectable.cs
@@ -4,10 +4,26 @@ using UnityEngine.Events;
 public class Collectable : MonoBehaviour
 {
     [SerializeField] private bool onlyTriggerOnPlayer = true;
+    [SerializeField] private AudioClip collectSfx;
+    [SerializeField, Range(0f, 1f)] private float collectSfxVolume = 1f;
     [SerializeField] private UnityEvent onCollect;
+
+    private bool hasBeenCollected;
 
     public void Collect()
     {
+        if (hasBeenCollected)
+            return;
+
+        hasBeenCollected = true;
+
+        if (collectSfx != null)
+            AudioSource.PlayClipAtPoint(
+                collectSfx,
+                transform.position,
+                collectSfxVolume
+            );
+
         onCollect.Invoke();
     }
 
